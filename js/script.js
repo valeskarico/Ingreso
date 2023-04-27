cd19262007ae49f57e7dab52af858b930ffa4755
valeskarico <124623365+valeskarico@users.noreply.github.com>
2023-04-26 21:50:44 -0300

    Actualizacion
    Personalizacion de input radio. Modificacion de espacios. Modificacion de funcionalidad

--- js/script.js
+++ js/script.js
@@ -1,48 +1,64 @@
 function validarAcceso() {
+
   var email = document.getElementById("email").value;
   var rol = document.querySelector('input[name="rol"]:checked').value;
 
-  if (email === "soyprofesor@email.com" && rol === "teacher") {
+
+  if (email === "profesor@email.com" && rol === "teacher") {
+
     window.location.href = "https://valeskarico.github.io/prueba-profesor/";
-  } else if (email === "soyestudiante@email.com" && rol === "student") {
+  } else if (email === "estudiante@email.com" && rol === "student") {
+
     window.location.href = "https://valeskarico.github.io/carga-video/";
   } else {
     event.preventDefault();
   }
 }
+
+
 const emailInput = document.getElementById('email');
 const rolInputs = document.getElementsByName('rol');
 
+
 formulario.addEventListener('submit', (event) => {
+
   event.preventDefault();
+
 
   const email = emailInput.value.trim();
 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   if (!emailRegex.test(email)) {
 
     alert('El correo electrónico ingresado no es válido');
-  } else if (email === 'soyprofesor@email.com') {
+  } else if (email === 'profesor@email.com') {
 
     if (rolInputs[0].checked) {
+
       alert('Bienvenido Profesor');
     } else if (rolInputs[1].checked) {
-      alert('El correo electrónico ingresado solo se permite para el rol de profesor');
+
+      alert('El correo electrónico ingresado solo se permite para el rol de Profesor');
     } else {
+
       alert('Seleccione un rol de usuario');
     }
+  } else if (email === 'estudiante@email.com') {
 
-  } else if (email === 'soyestudiante@email.com') {
     if (rolInputs[1].checked) {
+
       alert('Bienvenido Estudiante');
     } else if (rolInputs[0].checked) {
-      alert('El correo electrónico ingresado solo se permite para el rol de estudiante');
-    } else {   
+ 
+      alert('El correo electrónico ingresado solo se permite para el rol de Estudiante');
+    } else {
+
       alert('Seleccione un rol de usuario');
     }
   } else {
-    alert('Lo siento, su correo no esta en nuestra base de datos');
+    
+    alert('Lo siento, su correo electronico no esta en nuestra base de datos');
   }
 });
